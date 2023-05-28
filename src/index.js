e941300676e044ac294e4efb2bedeb04ebe57f06
AlexTS3 <tsvetanovalex@yahoo.com>
2023-05-28 12:33:21 +0300

    Created config folder and 404 page
    Express and handlebars configurations moved to seperate folder

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const path = require('path');
-const handlebars = require('express-handlebars');
+
+const handlebarsConfig = require('./config/handlebarsConfig');
+const expressConfig = require('./config/expressConfig');
 const routes = require('./router');
 
 const app = express();
 const PORT = 5000;
-
-function expressConfig(app) {
-    app.use(express.static(path.resolve(__dirname, '../public')));
-    app.use(express.urlencoded({ extended: false }));
-};
-
-function handlebarsConfig(app) {
-    app.engine('hbs', handlebars.engine({
-        extname: 'hbs',
-    }));
-    app.set('view engine', 'hbs');
-    app.set('views', 'src/views');
-}
 
 handlebarsConfig(app);
 expressConfig(app);
