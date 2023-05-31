023f1ed942aa7a6080df4f36f597a4bf32ba1d1d
AlexTS3 <tsvetanovalex@yahoo.com>
2023-05-31 16:15:42 +0300

    Added destinations page

--- src/router.js
+++ src/router.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 
 router.use(homeController);
+router.use('/places', (req, res) => {
+    res.render('places');
+});
 router.use('/search', (req, res) => {
     res.render('search');
 });
-router.use('*', (req,res) => {
+router.use('*', (req, res) => {
     res.redirect('/404')
 })
 
