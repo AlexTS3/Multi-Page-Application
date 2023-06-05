9d8d1199c430121d665349afcc5606a1ea677ff3
AlexTS3 <tsvetanovalex@yahoo.com>
2023-06-06 00:06:51 +0300

    Connected to db

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 
 const handlebarsConfig = require('./config/handlebarsConfig');
 const expressConfig = require('./config/expressConfig');
+const dbConnect = require('./config/dbConfig')
 const routes = require('./router');
 
 const app = express();
 const PORT = 5000;
+
+dbConnect()
+    .then(() => console.log('DB connected successfuly'))
+    .catch(err => { console.log('DB error', err) })
 
 handlebarsConfig(app);
 expressConfig(app);
